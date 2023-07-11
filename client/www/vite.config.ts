4eda8da9acc543dc95d8c8a2e2c6e8f837dfac8d
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-07-11 15:21:12 +0530

    proxy request "api" to dev server for development

--- client/www/vite.config.ts
+++ client/www/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:8000",
+    },
+  },
   plugins: [react()],
-})
+});
