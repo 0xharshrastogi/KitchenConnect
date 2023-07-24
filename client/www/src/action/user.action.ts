be3b4fb1c5003574fefe87385323cc7d490105a6
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-07-24 17:12:29 +0530

    removed unreachable code

--- client/www/src/action/user.action.ts
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { api } from "../api";
-import { UserCredential } from "../common/shared";
-
-const login = createAsyncThunk("user/login-user", async (credential: UserCredential) => {
-  return await api.auth.login(credential);
-});
-
-export const authAction = { login };
--- client/www/src/reducers/index.ts
+++
@@ -1,1 +0,0 @@
-export { actions as userActions, reducer as userReducer } from "./user";
